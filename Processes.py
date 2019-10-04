17d76382d7f44ae3a6967dbfe8b725e37d06404f
Your Name <you@example.com>
2019-10-04 19:42:38 +0530

    ofc

--- Processes.py
+++ Processes.py
@@ -12,16 +12,124 @@ import cv2
 from PIL import Image
 import math
 
-########################################################################################################################################
-# Importing user functions
-# from PreProcessing import get_coord
-# from PreProcessing import preprocessing_image
-
-# set path where invoice is
-#os.chdir(r'//home/tanmay/Desktop/MyProjects/InvoiceOCR/Invoices')
-os.chdir(r'/home/tanmay/Desktop/My Projects/ResumeOCR/Resume')
+# Functions to filter out the required text:
+# 1. FindReg - Just use regex to filter
+# 2. FindList - Use the regex and grammar to search if regex specified and
+#               Use the mentioned file to pick the specified names
+# 3. FindSec - Filter the text string for the specified section 
+# 4. 
+# 5. GetSections - To get all the sections present in the resume
+# 6. Extract data from image or pdfs
 
 #########################################################################################################################################
+# 1. FindAbs - Just use regex to filter
+# Find mentioned fields values using mapping
+
+def FindReg(textstr, field_match):
+
+    Pattern = re.compile(field_match[2])
+    matches = Pattern.findall(textstr)
+    
+    print(matches)
+    return(matches)
+
+#########################################################################################################################################
+
+# 2. FindList - Use the regex and grammar to search if regex specified and
+#               Use the mentioned file to pick the specified names
+
+def Findlist(textstr, field_match, matches):
+
+    # Reading file to match the master value list
+    file_name = field_match[7]
+    mas_list = pd.read_csv(file_name)
+
+    # Declaring variables to store final results    
+    ind = 0
+    col_names=['Field','Match_Field','text']
+    MatchedValues = pd.DataFrame(index=range(1,1000), columns=col_names)
+
+    # if regex is present
+    if len(matches) != '':
+        
+        #comparing matched values against the read master values
+        for i in matches:
+
+            match_master = mas_list.loc[mas_list['Values'] == i]
+
+            if len(match_master) != 0:
+
+                MatchedValues.iloc[ind,0]= field_match[0]
+                MatchedValues.iloc[ind,1]= field_match[1]
+                MatchedValues.iloc[ind,2] = i
+    else:
+        
+        for i in mas_list['Values']:
+            
+            loc = textstr.find(i)
+
+            if loc>0:
+            
+                MatchedValues.iloc[ind,0]= field_match[0]
+                MatchedValues.iloc[ind,1]= field_match[1]
+                MatchedValues.iloc[ind,2] = i
+
+    print(MatchedValues)
+    return(MatchedValues)
+
+#########################################################################################################################################
+
+# 3. FindSec - it will filter the text for a particular section
+def FindSec(textstr, field_match, next_section):
+
+    # pick the section to be filtered
+    section = field_match[8]
+
+    # extract words
+    blob = blob(textstr)
+    extracted_text =''
+
+    section_found = 0
+
+    for i in range(0,len(blob.words)):
+
+        if (blob.words[i] == section) or (blob.words[i] + ' ' + blob.words[i+1] == section) or (blob.words[i] + ' ' + blob.words[i+1] + ' ' + blob.words[i+2] == section) :
+            section_found=1
+        
+        if section_found == 1:
+
+            if (blob.words[i] == next_section) or (blob.words[i] + ' ' + blob.words[i+1] == next_section) or (blob.words[i] + ' ' + blob.words[i+1] + ' ' + blob.words[i+2] == next_section) :
+
+                extracted_text = extracted_text + blob.words[i] + ' '
+
+    return()
+
+#########################################################################################################################################
+
+# 5. To get all the sections present in the resume
+
+def GetSections(textstr,section):
+    
+    # Finding sections in the resume
+    xlsx = pd.ExcelFile('../Master_Data.xlsx')
+    sec = pd.read_excel(xlsx, 'Sections')
+    
+    all_sections = []
+    ind=0
+
+    # Filtering the sections
+    for i in sec['Keywords']:
+        
+        loc = textstr.find(i)
+
+        if loc>0:
+            all_sections[ind]=i
+            ind=ind+1
+
+    return(all_sections)
+
+#########################################################################################################################################
+# 6. to extract text from image or pdfs
 
 def extract_text(filename):
 
@@ -57,174 +165,3 @@ def extract_text(filename):
         return(textstr)
 
 #########################################################################################################################################
-
-#########################################################################################################################################
-
-# Finding text based on following types:
-# 1. FindAbs - Just use regex to filter
-# 2. FindList - Use the regex and grammar to search if regex specified and
-#               Use the mentioned file to pick the specified names
-# 3. NLTK - Use the regex and grammar to search
-# 4. None of the above - Search for the keyword and use text after it using Regex
-
-
-# Find mentioned fields values using mapping
-def find_text(textstr, filename):
-
-        # Reading csv containing all master data related to fields
-        xlsx = pd.ExcelFile('../Master_Data.xlsx')
-        df = pd.read_excel(xlsx, 'Field_Match')
-        df_key = pd.read_excel(xlsx, 'Sections')
-
-        # Reading index and text from passed string to extract each sentence
-        for txt,text in textstr.iteritems():
-                
-                #Printing Name
-                print("Name of the candidate: ")
-                if len(text.split('\n')[0])>2:
-                        print(text.split('\n')[0])
-                elif len(text.split('\n')[1])>2:
-                        print(text.split('\n')[1])
-                
-                for index,nu in df.iterrows():
-                        
-                        field = df.iloc[index,0]
-                        Match = df.iloc[index,1]
-                        regex = df.iloc[index,2]
-
-                        if field == 'FindAbs':
-                                Pattern = re.compile(regex)
-                                matches = Pattern.findall(text)
-                                print(matches)
-
-                #Printing blocks from resume
-                #for i in df_key.iterrows():
-                        
-
-        # Reading index and text from passed string to extract each sentence
-        for txt,text in textstr.iteritems():
-                # Creating textblob to traverse through words
-                blob = TextBlob(text)
-
-                # Calling read items to return items list
-                #read_items(blob, filename)
-
-                ind = 0
-                col_names=['Field','Match_Field','text']
-                MatchedValues = pd.DataFrame(index=range(1,1000), columns=col_names)
-                pos=0
-                word=''
-
-                #Iterating through each word in the blob
-                #try:
-                for word in blob.words:
-
-                        # Iterating through each row in master data rules
-                        for index, row in df.iterrows():
-
-                                #Assigning varibles from excel sheet
-
-                                field = df.iloc[index,0]
-                                Match = df.iloc[index,1]
-                                regex = df.iloc[index,2]
-
-                                priority = df.iloc[index,5]
-
-                                # Setting filter regex
-                                try:
-                                        if math.isnan(df.iloc[index,3]) == False:
-                                                filter_regex = df.iloc[index,3]
-                                        else: filter_regex=''
-                                except: filter_regex = df.iloc[index,3]
-
-                                # Setting filter values
-                                try:
-                                        if math.isnan(df.iloc[index,6]) == False:
-                                                skip_values = df.iloc[index,6]
-                                        else: skip_values=''
-                                except: skip_values = df.iloc[index,6]
-
-                                # Setting replace text
-                                try:
-                                        if math.isnan(df.iloc[index,4]) == False:
-                                                replace_text = df.iloc[index,4]
-                                        else: replace_text=''
-                                except: replace_text = df.iloc[index,4]
-
-                                skip=True
-
-                                matchtext=''
-                                txtlen = len(field.split())
-
-                                # Creating text as per the length of field to be compared
-                                for i in range(0,txtlen):
-                                        if i > 0:
-                                                matchtext = matchtext + ' '
-                                        matchtext = matchtext + blob.words[pos+i].upper()
-                                
-                                if (matchtext==field.upper()):
-
-                                        pattern = re.compile(regex)
-                                        for SubIndex in range(0,2):
-                                                matchflag = re.search(pattern, blob.words[pos+txtlen+SubIndex])
-
-                                                # Replacing phrases to clean the text
-                                                after_replace_text =  replace_phrases(blob.words[pos+txtlen+SubIndex], replace_text)
-
-                                                # Filtering text based on regex and values
-                                                skip = refine_matches(after_replace_text, filter_regex, skip_values)
-                                                if matchflag and skip:
-                                                        break
-
-                                        # If perfect match then save the entry
-                                        if matchflag:
-                                                MatchedValues.iloc[ind,0]=field
-                                                MatchedValues.iloc[ind,1]=Match
-                                                MatchedValues.iloc[ind,2]= after_replace_text
-                                                ind = ind + 1
-
-                        pos = pos + 1
-                #except:
-                #        print('skipped')
-
-        return(MatchedValues)
-
-#########################################################################################################################################
-
-#########################################################################################################################################
-
-#function to refine multiple regex matches and values
-# True  - means none of the filters matched
-# False - means filter applied and not to be considered
-def refine_matches(text,filter_regex, filter_values):
-
-        skip = True
-        if str(filter_values) != '' :
-                # Check filter text
-                for filter in filter_values.split('|'):
-                        if text == filter:
-                                skip = False
-
-        # Check filter regex
-        if str(filter_regex) != '' :
-                for regex in filter_regex.split('|'):
-                        pattern = re.compile(regex)
-                        matchflag = re.search(pattern, text)
-                        if matchflag:
-                                skip = False
-        return(skip)
-
-#########################################################################################################################################
-# Function to replace text
-def replace_phrases(text, replace_text):
-
-        final = True
-        if str(replace_text) != '' and replace_text != None:
-                # Check filter text
-                for phrase in replace_text.split('|'):
-                        text = text.replace(phrase,'')
-
-        return(text)
-
-#########################################################################################################################################
-
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from datetime import datetime
 # Importing user defined functions
 from Processes import extract_text
 from Processes import find_text
+from Processes import find_Abs
+
 
 def process_resume(filename):
 
@@ -13,7 +15,6 @@ def process_resume(filename):
 
     # extract_text(r'..//home/tanmay/Desktop/MyProjects/InvoiceOCR/Invoices/Invoice_Exp_Dec001_Veneklasen.pdf')
     textstr = extract_text(filename)
-
 
     # Reading csv containing all master data related to fields
     xlsx = pd.ExcelFile('../Master_Data.xlsx')
@@ -23,8 +24,7 @@ def process_resume(filename):
     for index,nu in df.iterrows():
 
         df_filter = df.
-    
-    
+
     #print csv  
     textstr.to_csv('Resume.csv')
 
